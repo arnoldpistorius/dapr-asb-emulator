0f8631c0ca89d6852a9ad44353d63fed12f6a380
Arnold Pistorius <arnold.pistorius@anywhere365.net>
2023-03-03 10:30:50 +0100

    Temporary fix for GRPC host

--- src/DaprAsbEmulator/Program.cs
+++ src/DaprAsbEmulator/Program.cs
@@ -2,8 +2,11 @@
 using DaprAsbEmulator.Adapter.Memory;
 using DaprAsbEmulator.Application;
 using DaprAsbEmulator.Ports;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2));
 
 var services = builder.Services;
 
@@ -15,16 +18,16 @@ services.AddSwaggerDocument()
     .AddSingleton<ISubscriptionRepository>(svc => svc.GetRequiredService<InMemoryTopicRepository>())
     .AddSingleton<ITopicSubscriptionEvents>(svc => svc.GetRequiredService<InMemoryTopicRepository>())
     .AddTransient<IValidatorService, ValidatorService>()
-    .AddControllers()
-    .Services
+    // .AddControllers()
+    // .Services
     .AddGrpc();
 
 var app = builder.Build();
 
 app.MapGrpcService<TopicsController>();
-app.MapControllers();
-app.UseOpenApi();
-app.UseSwaggerUi3();
+// app.MapControllers();
+// app.UseOpenApi();
+// app.UseSwaggerUi3();
 
 await app.RunAsync();
 
